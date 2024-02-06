b3c0fea7e8816708e48987609deb6c03d27957e8
mahendrakumarpaswan <mahendrapaswan1994@gmail.com>
2024-02-06 16:17:30 +0530

    changessomefiles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return <div>
+
+                 <h2>Welcome to Thapa React E-Commerce Website</h2>
+       
+        </div>;
 };
 
 export default App;
